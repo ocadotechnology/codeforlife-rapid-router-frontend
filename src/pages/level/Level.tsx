31eba0a6c321e15741c2482a06970fa5cb8f482d
Daniil Polovinkin <daniil.polovinkin@dataart.com>
2025-10-28 11:29:03 +0000

    feat(level page): add basic cookie

--- src/pages/level/Level.tsx
+++ src/pages/level/Level.tsx
@@ -1,6 +1,7 @@
 import * as yup from "yup"
 import { Box, Typography } from "@mui/material"
-import { type FC, useState } from "react"
+import { type FC, useEffect, useState } from "react"
+import Cookies from "js-cookie"
 import { useParamsRequired } from "codeforlife/hooks"
 
 import Controls from "./Controls"
@@ -8,12 +9,43 @@ import { paths } from "../../routes"
 
 export interface LevelProps {}
 
+const COOKIE_NAME = "Rapid Router"
+
+const TwoPanelsLayoutOptions = {
+  Auto: "2.Auto",
+  VerticalSplit: "2.VerticalSplit",
+  HorizontalSplit: "2.HorizontalSplit",
+} as const
+
+// Value temporarily unused
+// eslint-disable-next-line
+const ThreePanelsLayoutOptions = {
+  Auto: "3.Auto",
+  VerticalSplitWithLeftHorizontal: "3.VerticalSplitWithLeftHorizontal",
+  VerticalSplit: "3.VerticalSplit",
+  HorizontalSplit: "3.HorizontalSplit",
+} as const
+
+type LayoutOption =
+  | (typeof ThreePanelsLayoutOptions)[keyof typeof ThreePanelsLayoutOptions]
+  | (typeof TwoPanelsLayoutOptions)[keyof typeof TwoPanelsLayoutOptions]
+
 interface LevelState {
   panels: 2 | 3
+  layoutOption: LayoutOption
 }
 
 const Level: FC<LevelProps> = () => {
-  const [level] = useState<LevelState>({ panels: 2 })
+  const [level] = useState<LevelState>({
+    panels: 2,
+    layoutOption: TwoPanelsLayoutOptions.Auto,
+  })
+
+  useEffect(() => {
+    if (!Cookies.get(COOKIE_NAME)) {
+      Cookies.set(COOKIE_NAME, JSON.stringify(level))
+    }
+  }, [level])
 
   return useParamsRequired({
     shape: { id: yup.number().required().min(1) },
