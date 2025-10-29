3d714018a5f87b63173a43ed1935453666d39075
Daniil Polovinkin <daniil.polovinkin@dataart.com>
2025-10-29 09:15:49 +0000

    refactor(level): extract cookies settings to hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -5,9 +5,40 @@
 // for importing and re-exporting the typed versions of hooks.
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { useDispatch, useSelector } from "react-redux"
+import Cookies from "js-cookie"
 
 import type { AppDispatch, RootState } from "./store"
+import packageJson from "../../package.json"
+
+export type TwoPanelLayout = "auto" | "vertical" | "horizontal"
+export type ThreePanelLayout =
+  | "auto"
+  | "verticalWithLeftHorizontal"
+  | "vertical"
+  | "horizontalSplit"
+
+export interface Settings {
+  twoPanelLayout: TwoPanelLayout
+  threePanelLayout: ThreePanelLayout
+}
+
+const COOKIE_NAME = packageJson.name
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
+
+export const DEFAULT_SETTINGS: Settings = Object.freeze({
+  twoPanelLayout: "auto",
+  threePanelLayout: "auto",
+})
+
+export function useSettings(): Settings {
+  const settings = Cookies.get(COOKIE_NAME)
+  if (settings) {
+    return JSON.parse(settings) as Settings
+  } else {
+    Cookies.set(COOKIE_NAME, JSON.stringify(DEFAULT_SETTINGS))
+    return DEFAULT_SETTINGS
+  }
+}
--- src/pages/level/Level.tsx
+++ src/pages/level/Level.tsx
@@ -1,51 +1,24 @@
 import * as yup from "yup"
 import { Box, Typography } from "@mui/material"
-import { type FC, useEffect, useState } from "react"
-import Cookies from "js-cookie"
+import { type FC, useState } from "react"
 import { useParamsRequired } from "codeforlife/hooks"
 
 import Controls from "./Controls"
 import { paths } from "../../routes"
+import { useSettings } from "../../app/hooks"
 
 export interface LevelProps {}
 
-const COOKIE_NAME = "Rapid Router"
-
-const TwoPanelsLayoutOptions = {
-  Auto: "2.Auto",
-  VerticalSplit: "2.VerticalSplit",
-  HorizontalSplit: "2.HorizontalSplit",
-} as const
-
-// Value temporarily unused
-// eslint-disable-next-line
-const ThreePanelsLayoutOptions = {
-  Auto: "3.Auto",
-  VerticalSplitWithLeftHorizontal: "3.VerticalSplitWithLeftHorizontal",
-  VerticalSplit: "3.VerticalSplit",
-  HorizontalSplit: "3.HorizontalSplit",
-} as const
-
-type LayoutOption =
-  | (typeof ThreePanelsLayoutOptions)[keyof typeof ThreePanelsLayoutOptions]
-  | (typeof TwoPanelsLayoutOptions)[keyof typeof TwoPanelsLayoutOptions]
-
 interface LevelState {
   panels: 2 | 3
-  layoutOption: LayoutOption
 }
 
 const Level: FC<LevelProps> = () => {
   const [level] = useState<LevelState>({
     panels: 2,
-    layoutOption: TwoPanelsLayoutOptions.Auto,
   })
 
-  useEffect(() => {
-    if (!Cookies.get(COOKIE_NAME)) {
-      Cookies.set(COOKIE_NAME, JSON.stringify(level))
-    }
-  }, [level])
+  const settings = useSettings()
 
   return useParamsRequired({
     shape: { id: yup.number().required().min(1) },
@@ -53,6 +26,7 @@ const Level: FC<LevelProps> = () => {
       <Box sx={{ display: "flex" }}>
         <Controls />
         <Box component="main" sx={{ flexGrow: 1 }}>
+          <Typography>Settings: {JSON.stringify(settings)}</Typography>
           <Typography>Level state: {JSON.stringify(level)}</Typography>
         </Box>
       </Box>
