09d85aa57c10b144ac3c0036b5e65248e287dead
SKairinos <skairinos@gmail.com>
2026-05-29 16:00:01 +0000

    fix scene exports

--- src/phaser/scenes/index.ts
+++ src/phaser/scenes/index.ts
@@ -1,19 +1,2 @@
-import CreateBoot from "./create/Boot"
-import CreateLevelCreator from "./create/LevelCreator"
-import CreatePreloader from "./create/Preloader"
-
-export const create = [CreateBoot, CreatePreloader, CreateLevelCreator]
-
-import PlayBoot from "./play/Boot"
-import PlayGameOver from "./play/GameOver"
-import PlayGameplay from "./play/Gameplay"
-import PlayHUD from "./play/HUD"
-import PlayPreloader from "./play/Preloader"
-
-export const play = [
-  PlayBoot,
-  PlayPreloader,
-  PlayHUD,
-  PlayGameplay,
-  PlayGameOver,
-]
+export { default as create } from "./create"
+export { default as play } from "./play"
