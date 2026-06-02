1e49a3846aeddee5f532dfb86cb3acde55e01521
SKairinos <skairinos@gmail.com>
2026-06-02 15:26:54 +0000

    comments

--- src/phaser/scenes/BaseLevel.tsx
+++ src/phaser/scenes/BaseLevel.tsx
@@ -10,6 +10,16 @@ export default class extends Phaser.Scene {
   obstacleLayer!: Phaser.Tilemaps.TilemapLayer | Phaser.Tilemaps.TilemapGPULayer
   sceneryObjects!: Phaser.GameObjects.GameObject[]
 
+  /**
+   * Creates the background layer of the tilemap using the specified tileset
+   * names. The background layer is typically the bottom-most layer of the
+   * tilemap and is rendered first, serving as the visual foundation for the
+   * level. It is important to create this layer before other layers to ensure
+   * proper rendering order.
+   *
+   * @param tilesetNames The names of the tilesets to use for the background
+   * layer.
+   */
   createBackgroundLayer(tilesetNames: string[]) {
     this.backgroundTilesets = tilesetNames.map(
       name => this.tilemap.addTilesetImage(name)!,
@@ -20,6 +30,14 @@ export default class extends Phaser.Scene {
     )!
   }
 
+  /**
+   * Creates the obstacle layer of the tilemap using the specified tileset
+   * names. The obstacle layer is typically rendered on top of the background
+   * layer and serves as the interactive or impassable elements in the level.
+   *
+   * @param tilesetNames The names of the tilesets to use for the obstacle
+   * layer.
+   */
   createObstacleLayer(tilesetNames: string[]) {
     this.obstacleTilesets = tilesetNames.map(
       name => this.tilemap.addTilesetImage(name)!,
@@ -30,6 +48,13 @@ export default class extends Phaser.Scene {
     )!
   }
 
+  /**
+   * Creates the scenery objects of the tilemap using the specified types.
+   * Scenery objects are typically rendered on top of both the background and
+   * obstacle layers and serve as decorative elements in the level.
+   *
+   * @param types The types of the scenery objects to create.
+   */
   createSceneryObjects(types: string[]) {
     this.sceneryObjects = this.tilemap.createFromObjects(
       "Scenery", // This is hardcoded for consistency.
@@ -41,6 +66,19 @@ export default class extends Phaser.Scene {
     )
   }
 
+  /**
+   * Creates the tilemap for the level using the specified key and tileset
+   * names.The tilemap is the main structure that holds all the layers and
+   * objects in the level. This method ensures that the layers and objects are
+   * created in the correct order for proper rendering.
+   *
+   * @param key The key of the tilemap to create.
+   * @param backgroundTilesetNames The names of the tilesets to use for the
+   * background layer.
+   * @param obstacleTilesetNames The names of the tilesets to use for the
+   * obstacle layer.
+   * @param sceneryObjectTypes The types of the scenery objects to create.
+   */
   createTilemap({
     key,
     backgroundTilesetNames,
