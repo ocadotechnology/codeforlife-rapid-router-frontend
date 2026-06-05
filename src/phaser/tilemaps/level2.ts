98cdb405dc119b485ae5346877b82f1f1a71b7f7
aedan15 <Aedanshields@example.com>
2026-06-05 16:20:33 +0000

    levels 2+3 in the new format

--- src/phaser/tilemaps/level2.ts
+++ src/phaser/tilemaps/level2.ts
@@ -1,167 +1,76 @@
-import type { TiledTileset } from "tiled-types"
-
 import { SVGs, Tilesets } from "../enums"
-import {
-  fillTileLayerRow,
-  makeBackgroundTileLayer,
-  makeObstaclesTileLayer,
-  makeSceneryObjectGroupLayer,
-  makeTilemap,
-  makeTileset,
-} from "."
+import { fillManyRows, fillRow, rotate } from "../utils/tileset"
+import { makeOrthogonalTilemap } from "."
 
-const backgroundTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Background.GRASS,
-    firstgid: Tilesets.GRASS,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.SNOW,
-    firstgid: Tilesets.SNOW,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.Road.STRAIGHT,
-    firstgid: Tilesets.ROAD_STRAIGHT,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.Road.DEAD_END,
-    firstgid: Tilesets.ROAD_DEAD_END,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const obstacleTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Obstacles.PIGEON,
-    firstgid: Tilesets.PIGEON,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Obstacles.TrafficLight.RED,
-    firstgid: Tilesets.TRAFFIC_LIGHT_RED,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Obstacles.TrafficLight.GREEN,
-    firstgid: Tilesets.TRAFFIC_LIGHT_GREEN,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const sceneryTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Scenery.TREE1,
-    firstgid: Tilesets.TREE1,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Scenery.TREE2,
-    firstgid: Tilesets.TREE2,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const backgroundTileLayer = makeBackgroundTileLayer([
-  // Row 1
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 2
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 3
-  [
-    Tilesets.GRASS, //CFC here
-    Tilesets.ROAD_STRAIGHT | Tilesets.Rotate[90],
-    Tilesets.ROAD_STRAIGHT | Tilesets.Rotate[90],
-    Tilesets.ROAD_STRAIGHT | Tilesets.Rotate[90],
-    Tilesets.ROAD_DEAD_END | Tilesets.Rotate[90], //house here
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-  ],
-  // Row 4
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 5
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 6
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 7
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 8
-  fillTileLayerRow(Tilesets.GRASS),
-])
-
-const obstaclesTileLayer = makeObstaclesTileLayer([
-  // Row 1
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 2
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 3
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 4
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 5
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 6
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 7
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 8
-  fillTileLayerRow(Tilesets.EMPTY),
-])
-
-const sceneryObjectGroupLayer = makeSceneryObjectGroupLayer([
-  {
-    type: SVGs.Scenery.TREE1,
-    x: 352,
-    y: 96,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
+export default makeOrthogonalTilemap({
+  tilesets: {
+    background: [
+      {
+        image: SVGs.Background.GRASS,
+        firstgid: Tilesets.Background.GRASS,
+      },
+    ],
+    road: [
+      {
+        image: SVGs.Road.Asphalt.STRAIGHT,
+        firstgid: Tilesets.Road.Asphalt.STRAIGHT,
+      },
+      {
+        image: SVGs.Road.Asphalt.DEAD_END,
+        firstgid: Tilesets.Road.Asphalt.DEAD_END,
+      },
+    ],
+    environment: [
+      {
+        image: SVGs.Environment.Grass.CFC,
+        firstgid: Tilesets.Environment.Grass.CFC,
+      },
+    ],
+    scenery: [
+      {
+        image: SVGs.Scenery.TREE1,
+        firstgid: Tilesets.Scenery.TREE1,
+      },
+    ],
   },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 48,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
+  layers: {
+    background: {
+      // Rows 1 to 8 - 10 columns of grass tiles
+      data: fillManyRows({ tileset: Tilesets.Background.GRASS }),
+    },
+    road: {
+      data: [
+        // Row 1 to 2 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 2 }),
+        // Row 3
+        [
+          // 2 columns of straight road tiles rotated 90° to be horizontal
+          ...fillRow({
+            tileset: rotate(Tilesets.Road.Asphalt.STRAIGHT, 90),
+            cols: 3,
+          }),
+          rotate(Tilesets.Road.Asphalt.DEAD_END, 90),
+          ...fillRow({ cols: 7 }), // 7 columns of empty tiles
+        ],
+        // Row 4 to 8 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 5 }),
+      ],
+    },
+    environment: {
+      data: [
+        // Row 1 to 2 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 2 }),
+        // Row 3
+        [
+          rotate(Tilesets.Environment.Grass.CFC, 90),
+          ...fillRow({ cols: 9 }), // 9 columns of empty tiles
+        ],
+        // Row 4 to 8 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 5 }),
+      ],
+    },
+    scenery: {
+      objects: [{ type: SVGs.Scenery.TREE1, x: 48, y: 208 }],
+    },
   },
-])
-
-export default makeTilemap({
-  tilesets: [...backgroundTilesets, ...obstacleTilesets, ...sceneryTilesets],
-  layers: [backgroundTileLayer, obstaclesTileLayer, sceneryObjectGroupLayer],
 })
--- src/phaser/tilemaps/level3.ts
+++ src/phaser/tilemaps/level3.ts
@@ -1,258 +1,101 @@
-import type { TiledTileset } from "tiled-types"
-
 import { SVGs, Tilesets } from "../enums"
-import {
-  fillTileLayerRow,
-  makeBackgroundTileLayer,
-  makeObstaclesTileLayer,
-  makeSceneryObjectGroupLayer,
-  makeTilemap,
-  makeTileset,
-} from "."
+import { fillManyRows, fillRow, rotate } from "../utils/tileset"
+import { makeOrthogonalTilemap } from "."
 
-const backgroundTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Background.GRASS,
-    firstgid: Tilesets.GRASS,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.SNOW,
-    firstgid: Tilesets.SNOW,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.Road.STRAIGHT,
-    firstgid: Tilesets.ROAD_STRAIGHT,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.Road.DEAD_END,
-    firstgid: Tilesets.ROAD_DEAD_END,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Background.Road.TURN,
-    firstgid: Tilesets.ROAD_TURN,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const obstacleTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Obstacles.PIGEON,
-    firstgid: Tilesets.PIGEON,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Obstacles.TrafficLight.RED,
-    firstgid: Tilesets.TRAFFIC_LIGHT_RED,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Obstacles.TrafficLight.GREEN,
-    firstgid: Tilesets.TRAFFIC_LIGHT_GREEN,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const sceneryTilesets: TiledTileset[] = [
-  makeTileset({
-    image: SVGs.Scenery.TREE1,
-    firstgid: Tilesets.TREE1,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-  makeTileset({
-    image: SVGs.Scenery.TREE2,
-    firstgid: Tilesets.TREE2,
-    tilewidth: 64,
-    tileheight: 64,
-    imagewidth: 64,
-    imageheight: 64,
-  }),
-]
-
-const backgroundTileLayer = makeBackgroundTileLayer([
-  // Row 1
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 2
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 3
-  [
-    Tilesets.GRASS, //CFC here
-    Tilesets.ROAD_STRAIGHT | Tilesets.Rotate[90],
-    Tilesets.ROAD_TURN,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-  ],
-  // Row 4
-  [
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.ROAD_DEAD_END | Tilesets.Rotate[180], //house here
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-    Tilesets.GRASS,
-  ],
-  // Row 5
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 6
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 7
-  fillTileLayerRow(Tilesets.GRASS),
-  // Row 8
-  fillTileLayerRow(Tilesets.GRASS),
-])
-
-const obstaclesTileLayer = makeObstaclesTileLayer([
-  // Row 1
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 2
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 3
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 4
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 5
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 6
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 7
-  fillTileLayerRow(Tilesets.EMPTY),
-  // Row 8
-  fillTileLayerRow(Tilesets.EMPTY),
-])
-
-const sceneryObjectGroupLayer = makeSceneryObjectGroupLayer([
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 0,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
+export default makeOrthogonalTilemap({
+  tilesets: {
+    background: [
+      {
+        image: SVGs.Background.GRASS,
+        firstgid: Tilesets.Background.GRASS,
+      },
+    ],
+    road: [
+      {
+        image: SVGs.Road.Asphalt.STRAIGHT,
+        firstgid: Tilesets.Road.Asphalt.STRAIGHT,
+      },
+      {
+        image: SVGs.Road.Asphalt.DEAD_END,
+        firstgid: Tilesets.Road.Asphalt.DEAD_END,
+      },
+      {
+        image: SVGs.Road.Asphalt.TURN,
+        firstgid: Tilesets.Road.Asphalt.TURN,
+      },
+    ],
+    environment: [
+      {
+        image: SVGs.Environment.Grass.CFC,
+        firstgid: Tilesets.Environment.Grass.CFC,
+      },
+    ],
+    scenery: [
+      {
+        image: SVGs.Scenery.TREE1,
+        firstgid: Tilesets.Scenery.TREE1,
+      },
+      {
+        image: SVGs.Scenery.TREE2,
+        firstgid: Tilesets.Scenery.TREE2,
+      },
+    ],
   },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 64,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
+  layers: {
+    background: {
+      // Rows 1 to 8 - 10 columns of grass tiles
+      data: fillManyRows({ tileset: Tilesets.Background.GRASS }),
+    },
+    road: {
+      data: [
+        // Row 1 to 2 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 2 }),
+        // Row 3
+        [
+          // 2 columns of straight road tiles rotated 90° to be horizontal
+          ...fillRow({
+            tileset: rotate(Tilesets.Road.Asphalt.STRAIGHT, 90),
+            cols: 2,
+          }),
+          Tilesets.Road.Asphalt.TURN,
+          ...fillRow({ cols: 7 }),
+        ],
+        //row 4
+        [
+          ...fillRow({ cols: 2 }),
+          rotate(Tilesets.Road.Asphalt.DEAD_END, 180),
+          ...fillRow({ cols: 7 }),
+        ],
+        // Row 5 to 8 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 4 }),
+      ],
+    },
+    environment: {
+      data: [
+        // Row 1 to 2 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 2 }),
+        // Row 3
+        [
+          rotate(Tilesets.Environment.Grass.CFC, 90),
+          ...fillRow({ cols: 9 }), // 9 columns of empty tiles
+        ],
+        // Row 4 to 8 - 10 columns of empty tiles
+        ...fillManyRows({ rows: 5 }),
+      ],
+    },
+    scenery: {
+      objects: [
+        { type: SVGs.Scenery.TREE2, x: 0, y: 64 },
+        { type: SVGs.Scenery.TREE2, x: 64, y: 64 },
+        { type: SVGs.Scenery.TREE2, x: 128, y: 68 },
+        { type: SVGs.Scenery.TREE2, x: 192, y: 64 },
+        { type: SVGs.Scenery.TREE2, x: 256, y: 68 },
+        { type: SVGs.Scenery.TREE2, x: 320, y: 60 },
+        { type: SVGs.Scenery.TREE2, x: 384, y: 64 },
+        { type: SVGs.Scenery.TREE2, x: 448, y: 68 },
+        { type: SVGs.Scenery.TREE2, x: 512, y: 60 },
+        { type: SVGs.Scenery.TREE2, x: 576, y: 64 },
+      ],
+    },
   },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 128,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 192,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 256,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 320,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 384,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 448,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 512,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-  {
-    type: SVGs.Scenery.TREE2,
-    x: 576,
-    y: 64,
-    width: 64,
-    height: 64,
-    rotation: 0,
-    properties: [],
-  },
-])
-
-export default makeTilemap({
-  tilesets: [...backgroundTilesets, ...obstacleTilesets, ...sceneryTilesets],
-  layers: [backgroundTileLayer, obstaclesTileLayer, sceneryObjectGroupLayer],
 })
