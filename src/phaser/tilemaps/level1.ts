2969fad972f9565a5b4ca5ab43b46c8d56ca563e
SKairinos <skairinos@gmail.com>
2026-06-11 08:44:03 +0000

    fix level 1

--- src/phaser/tilemaps/level1.ts
+++ src/phaser/tilemaps/level1.ts
@@ -2,32 +2,38 @@ import * as layers from "../layers"
 import * as objects from "../objects"
 import * as tilemaps from "./tilemaps"
 import * as tilesets from "../tilesets"
+import { TILE_HEIGHT, TILE_WIDTH } from "../constants"
 
 export default tilemaps.makeOrthogonal({
   tilesets: [
     tilesets.background.grass,
     tilesets.road.asphalt.straight,
     tilesets.road.asphalt.deadEnd,
-    tilesets.environment.common.trafficLight.red,
+    tilesets.environment.grass.cfc,
     tilesets.scenery.common.tree1,
-    tilesets.scenery.common.tree2,
   ],
   layers: {
-    // Rows 1 to 8 - 10 columns of grass tiles
     background: {
+      // Rows 1 to 8 - 10 columns of grass tiles
       data: layers.tile.fillManyRows({ id: tilesets.IDs.Background.GRASS }),
     },
     road: {
       data: [
-        // Row 1
+        // Row 1 to 2 - 10 columns of empty tiles
+        ...layers.tile.fillManyRows({ rows: 2 }),
+        // Row 3
         [
-          tilesets.IDs.EMPTY,
-          tilesets.rotateC(tilesets.IDs.Road.Asphalt.STRAIGHT, 90),
+          // 2 columns of straight road tiles rotated 90° to be horizontal
+          ...layers.tile.fillRow({
+            id: tilesets.rotateC(tilesets.IDs.Road.Asphalt.STRAIGHT, 90),
+            cols: 2,
+          }),
+          // 1 column of dead end road tile rotated 90° to be horizontal
           tilesets.rotateC(tilesets.IDs.Road.Asphalt.DEAD_END, 90),
-          ...layers.tile.fillRow({ cols: 7 }),
+          ...layers.tile.fillRow({ cols: 7 }), // 7 columns of empty tiles
         ],
-        // Row 2 to 8 - 10 columns of empty tiles
-        ...layers.tile.fillManyRows({ rows: 7 }),
+        // Row 4 to 8 - 10 columns of empty tiles
+        ...layers.tile.fillManyRows({ rows: 5 }),
       ],
     },
     environment: {
@@ -36,30 +42,21 @@ export default tilemaps.makeOrthogonal({
         ...layers.tile.fillManyRows({ rows: 2 }),
         // Row 3
         [
-          ...layers.tile.fillRow({ cols: 5 }), // 5 columns of empty tiles
-          tilesets.IDs.Environment.Common.TrafficLight.RED,
-          ...layers.tile.fillRow({ cols: 4 }), // 4 columns of empty tiles
-        ],
-        layers.tile.fillRow(), // Row 4 - 10 columns of empty tiles
-        // Row 5
-        [
-          ...layers.tile.fillRow({ cols: 2 }), // 2 columns of empty tiles
-          tilesets.IDs.Environment.Common.TrafficLight.RED,
-          ...layers.tile.fillRow({ cols: 7 }), // 7 columns of empty tiles
-        ],
-        // Row 6
-        [
+          // 1 column of CFC tile rotated 90° to be horizontal.
+          tilesets.rotateC(tilesets.IDs.Environment.Grass.CFC, 90),
           ...layers.tile.fillRow({ cols: 9 }), // 9 columns of empty tiles
-          tilesets.IDs.Environment.Common.TrafficLight.RED,
         ],
-        // Row 7 to 8 - 10 columns of empty tiles
-        ...layers.tile.fillManyRows({ rows: 2 }),
+        // Row 4 to 8 - 10 columns of empty tiles
+        ...layers.tile.fillManyRows({ rows: 5 }),
       ],
     },
     scenery: {
       objects: [
-        { ...objects.scenery.common.tree1, x: 128, y: 128 },
-        { ...objects.scenery.common.tree2, x: 256, y: 128 },
+        {
+          ...objects.scenery.common.tree1,
+          x: TILE_WIDTH * 1,
+          y: TILE_HEIGHT * 4,
+        },
       ],
     },
   },
